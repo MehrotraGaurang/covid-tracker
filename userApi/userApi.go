9e1a8707ce1752079d61684a16ebb9102633418e
Gaurang Mehrotra <gaurang.dtu@gmail.com>
2021-12-18 01:55:33 +0530

    Added separate file for location api

--- userApi/userApi.go
+++ userApi/userApi.go
@@ -3,16 +3,11 @@ package userApi
 import (
 	"app/cache"
 	"app/database"
-	fileutil "app/fileUtil"
-	"os"
-	"strings"
-
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/labstack/echo"
@@ -31,7 +26,7 @@ func getStateCount(c echo.Context) error {
 
 	fmt.Println("Getting StateCode: ", json_map)
 
-	stateCode := getStateCode(json_map["lat"], json_map["long"])
+	stateCode := GetStateCode(json_map["lat"], json_map["long"])
 
 	countData := cache.GetFromRedis(stateCode)
 
@@ -49,52 +44,6 @@ func getStateCount(c echo.Context) error {
 	retData := map[string]interface{}{stateCode: countData, "IN": countDataIndia}
 
 	return c.JSON(http.StatusOK, retData)
-}
-
-func getStateCode(lat string, long string) string {
-
-	url := fileutil.AppConfigProperties["reverse_geo"] + lat + "," + long
-
-	fmt.Println("Getting state code for lat long: " + lat + "," + long)
-	fmt.Println(url)
-
-	spaceClient := http.Client{
-		Timeout: time.Minute * 2, // Timeout after 2 minutes
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Set("User-Agent", "spacecount-tutorial")
-
-	res, getErr := spaceClient.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	if res.Body != nil {
-		defer res.Body.Close()
-	}
-
-	body, readErr := ioutil.ReadAll(res.Body)
-
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	var jsonMap map[string]interface{}
-
-	jsonErr := json.Unmarshal(body, &jsonMap)
-
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-
-	stateCode := jsonMap["data"].([]interface{})[0].(map[string]interface{})["region_code"].(string)
-
-	return stateCode
 }
 
 func Main() {
