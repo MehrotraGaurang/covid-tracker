3c17272133c6e4045cbcf9105458e5d23f835bac
Gaurang Mehrotra <gaurang.dtu@gmail.com>
2021-12-18 02:00:17 +0530

    Updated file management

---
+++ API/populateDb.go
@@ -0,0 +1,116 @@
+package API
+
+import (
+	"app/cache"
+	"app/constants"
+	"app/database"
+	"app/models"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func doEvery(d time.Duration, f func()) {
+	for range time.Tick(d) {
+		f()
+	}
+}
+
+func StartPopulating() {
+	doEvery(6*time.Hour, populateDb)
+}
+
+func populateDb() {
+
+	// fmt.Println(constants.StateCodes)
+
+	url := "https://api.rootnet.in/covid19-in/stats/latest"
+
+	spaceClient := http.Client{
+		Timeout: time.Minute * 2, // Timeout after 2 minutes
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	var jsonMap map[string]interface{}
+	jsonErr := json.Unmarshal(body, &jsonMap)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	var stateObjects []models.StateObject
+
+	lastUpdate, err := time.Parse(constants.Layout, jsonMap["lastRefreshed"].(string))
+
+	if err != nil {
+		fmt.Println(err)
+		lastUpdate = time.Now()
+	}
+
+	if isDataUpdatedAtApi(lastUpdate) {
+		var indiaObject models.StateObject
+
+		indiaObject.LastUpdated = lastUpdate
+		indiaObject.StateCode = "IN"
+		indiaObject.StateName = "India"
+		indiaObject.TotalNo = jsonMap["data"].(map[string]interface{})["summary"].(map[string]interface{})["total"].(float64)
+
+		stateObjects = append(stateObjects, indiaObject)
+
+		stateInfo := jsonMap["data"].(map[string]interface{})["regional"].([]interface{})
+
+		for _, value := range stateInfo {
+
+			var stateObject models.StateObject
+
+			value := value.(map[string]interface{})
+
+			stateName := value["loc"].(string)
+
+			stateObject.LastUpdated = lastUpdate
+			stateObject.StateCode = constants.StateCodes[stateName]
+			stateObject.StateName = stateName
+			stateObject.TotalNo = value["totalConfirmed"].(float64)
+
+			stateObjects = append(stateObjects, stateObject)
+		}
+
+		database.UpdateDb(stateObjects)
+
+	} else {
+		fmt.Printf("No new TimeStamp to update DB!")
+	}
+
+}
+
+func isDataUpdatedAtApi(lastUpdated time.Time) bool {
+
+	oldTs := cache.GetTs()
+
+	if oldTs.IsZero() || oldTs.Before(lastUpdated) {
+		cache.StoreTs(lastUpdated)
+		return true
+	}
+	return false
+}
---
+++ API/userApi.go
@@ -0,0 +1,67 @@
+package API
+
+import (
+	"app/cache"
+	"app/database"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func getStateCount(c echo.Context) error {
+
+	json_map := make(map[string]string)
+	err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Getting counts now")
+
+	fmt.Println("Getting StateCode: ", json_map)
+
+	stateCode := GetStateCode(json_map["lat"], json_map["long"])
+
+	countData := cache.GetFromRedis(stateCode)
+
+	if strings.Compare(countData.StateCode, "Not_Found") == 0 {
+
+		fmt.Println("Statecode not found, caching it: ", stateCode)
+
+		countData = database.GetCovidData(stateCode)
+		cache.StoreInRedis(countData, 30*time.Minute)
+
+	}
+
+	countDataIndia := database.GetCovidData("IN")
+
+	retData := map[string]interface{}{stateCode: countData, "IN": countDataIndia}
+
+	return c.JSON(http.StatusOK, retData)
+}
+
+func Main() {
+
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/count", getStateCount)
+
+	port := os.Getenv("PORT")
+
+	fmt.Println("Listening on", port)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":" + port))
+
+}
